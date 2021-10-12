8971e412dc326b9e58f550a609c378e91ba3154e
William Quayson <wgsquayson@gmail.com>
2021-10-12 17:21:48 -0300

    feat: added plopjs

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.main``;
+
+const Button = () => {
+  return (
+    <Wrapper>
+      <h1>Button</h1>
+    </Wrapper>
+  );
+};
+
+export default Button;
---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,10 @@
+import { Story, Meta } from "@storybook/react";
+
+import Button from ".";
+
+export default {
+  title: "Button",
+  component: Button
+} as Meta;
+
+export const Basic: Story = (args) => <Button {...args} />;
